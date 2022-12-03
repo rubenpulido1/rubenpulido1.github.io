cfa088c7d20cc685f5451bc8343399ade8a87ac9
Ruben Pulido <rubenpulido@berkeley.edu>
2022-12-03 02:21:50 -0800

    Deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-modern-personal-website.netlify.app',
+  site: 'https://rubenpulido1.github.io/',
   integrations: [mdx(), sitemap(), tailwind()]
 });
